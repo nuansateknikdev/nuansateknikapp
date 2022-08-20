dfd8b3b8383ac8b78d1ae95938f430bb730f7199
mikhlasnr <minrnullpo@gmail.com>
2022-08-20 11:58:25 +0700

    init file

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+   <div>
+      <Link href="/signin">
+          <a>signin</a>
+      </Link>
+   </div>
+  )
+}
---
+++ pages/signin.jsx
@@ -0,0 +1,10 @@
+
+const Signin = ({ language, metatags }) => {
+  return (
+    <div>
+      <h1>INI SIGNIN PAGE</h1>
+    </div>
+  )
+}
+  
+export default Signin
