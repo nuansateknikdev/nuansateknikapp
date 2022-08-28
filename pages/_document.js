82dccdd981ec61356081740ab5e0beee9f3c999e
mikhlasnr <minrnullpo@gmail.com>
2022-08-28 16:32:39 +0700

    comment crosorigin

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;600;700;800&display=swap"
             rel="stylesheet"></link>
