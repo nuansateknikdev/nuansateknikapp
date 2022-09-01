fb68807ca614c3bf5c53577f93bcea8ea58717ff
Ilham zaki <ilhamzaki32@gmail.com>
2022-09-01 17:14:35 +0700

    create modal from product

--- pages/produk.jsx
+++ pages/produk.jsx
@@ -1,12 +1,12 @@
-import ProdukMain from '../src/sections/Produk'
-import Layout from '../src/layout'
+import ProdukMain from '../src/sections/Produk';
+import Layout from '../src/layout';
 
 const Produk = () => {
   return (
-    <Layout id="produk-page" title="Produk">
+    <Layout id="produk-page" title="Produk" subTitle="Daftar nama-nama produk">
       <ProdukMain />
     </Layout>
-  )
-}
+  );
+};
 
-export default Produk
+export default Produk;
---
+++ src/sections/Produk/components/FormStok/index.jsx
@@ -0,0 +1,33 @@
+import { Button, Form, Input, InputNumber } from 'antd';
+
+const FormTambahStok = () => {
+  return (
+    <Form
+      // form={form}
+      layout={'vertical'}
+      // onFinish={onFinish}
+    >
+      <Form.Item label="Nama Produk" name="name">
+        <Input size="large" readOnly />
+      </Form.Item>
+      <Form.Item
+        label="Stok Barang"
+        name="stock"
+        initialValue={8}
+        extra="*stok saat ini"
+      >
+        <Input size="large" readOnly />
+      </Form.Item>
+      <Form.Item name="stock" extra="*lakukan penambahan stok disini">
+        <InputNumber size="large" style={{ width: '100%' }} />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+          Tambah
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default FormTambahStok;
